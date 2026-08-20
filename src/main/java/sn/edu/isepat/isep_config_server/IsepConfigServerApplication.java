172d7f5c5bb33a73cb8cd3f3dbd8da504393ff4b
Samba SIDIBE <sambasidibe@gmail.com>
2026-08-20 14:48:03 +0000

    v1

---
+++ src/main/java/sn/edu/isepat/isep_config_server/IsepConfigServerApplication.java
@@ -0,0 +1,15 @@
+package sn.edu.isepat.isep_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class IsepConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsepConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/isepat/isep_config_server/IsepConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.isep_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsepConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
